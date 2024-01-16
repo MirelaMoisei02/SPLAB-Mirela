39b7b49a104cbbbbd8b6ab71187f18cfe9152b70
Mirela <mirela.moisei02@e-uvt.ro>
2024-01-16 02:08:31 +0200

    lab 4 - proxy

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,33 +2,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+
+        long startTime = System.currentTimeMillis();
+        Image img1 = new Image("Pamela Anderson");
+        Image img2 = new Image("Kim Kardashian");
+        Image img3 = new Image("Kirby Griffin");
+        Section playboyS1 = new Section("Front Cover");
+        playboyS1.add(img1);
+        Section playboyS2 = new Section("Summer Girls");
+        playboyS2.add(img2);
+        playboyS2.add(img3);
+        Book playboy = new Book("Playboy");
+
+        playboy.addContent(playboyS1);
+        playboy.addContent(playboyS2);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Creation of the content took " + (endTime -
+                startTime) + " milliseconds");
+        startTime = System.currentTimeMillis();
+        playboyS1.print();
+        endTime = System.currentTimeMillis();
+        System.out.println("Printing of the section 1 took " + (endTime -
+                startTime) + " milliseconds");
+        startTime = System.currentTimeMillis();
+        playboyS1.print();
+        endTime = System.currentTimeMillis();
+        System.out.println("Printing again the section 1 took " + (endTime -
+                startTime) + " milliseconds");
         // Create a list to store books
-        List<Book> books = new ArrayList<>();
+        //  List<Book> books = new ArrayList<>();
 
         // Create and add books to the list
-        Book book1 = new Book("Disco Titanic");
-        Book book2 = new Book("Noapte buna, copii!");
-        Author author2 = new Author("Radu Pavel Gheo");
-        book2.addAuthor(author2);
+        // Book book1 = new Book("Divergent");
+        // Book book2 = new Book("Harry Potter");
+        // Author author2 = new Author("JK Rowling");
+        //book2.addAuthor(author2);
 
         // Create sections for each book
-        Section section1 = new Section("Introduction");
-        Section section2 = new Section("Chapter 1");
-        Section section3 = new Section("Prologue");
-        Section section4 = new Section("Chapter 2");
+        // Section section1 = new Section("Introduction");
+        //Section section2 = new Section("Chapter 1");
+        //Section section3 = new Section("Prologue");
+        //Section section4 = new Section("Chapter 2");
 
-        book2.add(section1);
+        //book2.add(section1);
 
         // Add books to the list
-        books.add(book1);
-        books.add(book2);
+        //books.add(book1);
+        //books.add(book2);
 
         // Print both books with their sections
-        for (Book book : books) {
-            book.print();
-        }
+        // for (Book book : books) {
+        //   book.print();
+        //  }
 
-        book1.print();
+        //book1.print();
     }
 }
